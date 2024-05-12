eecdf5feaa35c8e6c3bd5646afd02b9147a9c8c1
Tsagaadai'N <tsagaadain@gmail.com>
2024-05-12 11:11:28 +0800

    donedondneod

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
   }, []);
   const onSearch = async () => {
     try {
-      router.push("/search");
+      router.push("/Command/Search");
     } catch (error) {
       console.error("Error during login:", error);
     } finally {
@@ -35,24 +35,56 @@ export default function HomePage() {
   };
   return (
     <main>
-      <div className="text-white h-screen flex p-20  mx-0 md:mx-40 justify-around items-center">
-        {/* <div className="text-black font-sans text-lg">Search</div> */}
-        <ButtonDialog></ButtonDialog>
+      <div className="flex md:flex-row md:justify-center lg:px-12 gap-3  md:mt-5 flex-col px-3 md:px-5 mt-8">
+        <section className="md:w-[55%] flex flex-col w-[20%] justify-start px-3">
+          <section className="flex flex-col gap-5">
+            <h3 className="text-black font-bold xl:text-base  lg:text-sm md:text-2xl text-2xl">
+              Оюутны бүтээлийн бичиг баримт болон бусад бүтээлийг{" "}
+              <span className="text-[#245BFF]">нэг дороос</span>
+            </h3>
+            <p className="lg:text-[1rem] text-[#34444B] text-base ">
+              Оюутны бүтээлийн мэдээллийг хялбар,
+              <span className="font-bold pl-1">нэгдсэн байдлаар</span> харах, мөн
+              өөрсдийн бүтээлээ оруулж бусдад хуваалцах боломжтой.
+            </p>
+          </section>
+        </section>
+      </div>
+      <div className="text-white h-auto flex mx-5 md:mx-40 ml:mx-80 my-10 justify-around items-start ">
+        <div className="ms-12">
+          <Image
+            src="/image1.png"
+            alt="Vercel Logo"
+            width={225}
+            height={150}
+            priority
+            className="w-600 h-600 pt-12"
+          />
+          <ButtonDialog />
+        </div>
         <motion.div
           animate={{ scale: [0, 1, 0.5, 1] }}
           transition={{ times: [0, 1.1, 2.9, 4] }}
+          className="w-1000 h-1000"
         >
           <Image
             src="/3.png"
             alt="Vercel Logo"
-            width={650}
-            height={650}
-            priority
+            width={1000}
+            height={1000}
           />
         </motion.div>
-        <ButtonDialog2></ButtonDialog2>
-        {/* <div className="text-black font-sans text-lg">My lessons</div> */}
-
+        <div className="ms-12">
+          <Image
+            src="/image1.png"
+            alt="Vercel Logo"
+            width={225}
+            height={150}
+            priority
+            className="w-600 h-600 pt-12"
+          />
+          <ButtonDialog />
+        </div>
       </div>
     </main>
   );
--- src/components/ButtonDialog/index.tsx
+++ src/components/ButtonDialog/index.tsx
@@ -22,8 +22,8 @@ function ButtonDialog() {
     <section>
       <Dialog>
         <DialogTrigger asChild>
-          <button className="border-2 border-white/20 text-white bg-[#3364C3] font-semibold text-sm w-fit  shadow-[0px_0px_24px_0px_#799BFF] flex flex-row items-center gap-2 cursor-pointer rounded-xl  md:py-[0.6rem] md:px-[1rem] md:text-[1.2rem] py-[0.5rem] px-[1.2rem] text-[1.2rem] hover:bg-opacity-90">
-            <p className="text-white font-bold md:text-[1rem] text-[1.2rem]">
+          <button className=" border-white/20 text-black font-semibold text-sm w-fit  shadow-[0px_0px_24px_0px_#799BFF] flex flex-row items-center gap-2 cursor-pointer rounded-xl  md:py-[0.6rem] md:px-[1rem] md:text-[1.2rem] py-[0.5rem] px-[1.2rem] text-[1.2rem] hover:bg-opacity-80 transform transition-transform hover:scale-150">
+            <p className="text-black font-bold md:text-[1rem] text-[1.2rem]">
               Хичээлийн нэрээр хайх
             </p>
             <div className="text-2xl">
